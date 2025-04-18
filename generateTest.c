9ee2f4fe062d543917c0b58eadd16ef897fcec0d
orientiered <da.morgachev@mail.ru>
2025-04-18 15:51:49 +0300

    Removed scanf to make test generation faster

--- generateTest.c
+++ generateTest.c
@@ -13,6 +13,9 @@ int64_t getFileLen(FILE *file) {
 }
 
 const int MAX_WORD_LEN = 32;
+const int MIN_RNDWORD_LEN = 3;
+const int MAX_RNDWORD_LEN = 14;
+const int BUFFERING_SIZE = 1 << 18;
 
 int generateRandomWord(char *word, int minLen, int maxLen) {
     assert(minLen > 0);
@@ -26,31 +29,55 @@ int generateRandomWord(char *word, int minLen, int maxLen) {
     return len;
 }
 
+typedef struct {
+    char *data;
+    int64_t length;
+    char **words;
+    int64_t wordsCount;
+} text_t;
+
+text_t readFileSplit(const char *fileName);
+
+void writeTestsToFile(const char *fileName, text_t text, int tests, float foundPercent);
+
+
 int main(int argc, const char *argv[]) {
 
-    FILE *input = fopen(argv[1], "r");
-    FILE *output = fopen(argv[2], "w");
-
-    if (!input || !output) {
-        fprintf(stderr, "failed to open %s or %s\n", argv[1], argv[2]);
+    text_t text = readFileSplit(argv[1]);
+    if (!text.data) {
+        fprintf(stderr, "Failed to read file\n");
         return 1;
     }
 
     int totalRequests = 0; sscanf(argv[3], "%d", &totalRequests);
     float foundPercent = 0.; sscanf(argv[4], "%f", &foundPercent);
 
-    setvbuf(input, NULL, _IOFBF, 65536);
-    setvbuf(output, NULL, _IOFBF, 65536);
+    writeTestsToFile(argv[2], text, totalRequests, foundPercent);
 
-    int64_t textLen = getFileLen(input);
+    free(text.words);
+    free(text.data);
 
-    fprintf(stderr, "Len = %ji bytes\n", textLen);
+    return 0;
+}
 
-    char *text = (char*) calloc(textLen+1, 1);
-    char **words = (char **) calloc(textLen, sizeof(char *));
+text_t readFileSplit(const char *fileName) {
+    text_t result = {0};
 
-    assert(fread(text, 1, textLen, input) == textLen);
-    fclose(input);
+    FILE *file = fopen(fileName, "r");
+    if (!file) {
+        fprintf(stderr, "Failed to open %s\n", fileName);
+        return result;
+    }
+
+    result.length = getFileLen(file);
+
+    fprintf(stderr, "Len = %ji bytes\n", result.length);
+
+    char *text = (char*) calloc(result.length + 1, 1); // last byte serves as terminator
+    char **words = (char **) calloc(result.length, sizeof(char *));
+
+    assert(fread(text, 1, result.length, file) == result.length);
+    fclose(file);
 
     int64_t wordCount = 0;
     char *textPtr = text;
@@ -59,7 +86,10 @@ int main(int argc, const char *argv[]) {
     textPtr += shift;
     while (true) {
         int wordLen = 0;
-        sscanf(textPtr, "%*s%n %n", &wordLen, &shift);
+        while (*(textPtr + wordLen) && !isspace(*(textPtr+wordLen)) ) wordLen++;
+        shift = wordLen;
+        while(*(textPtr + shift) && isspace(*(textPtr + shift)) ) shift++;
+
         if (wordLen == 0)
             break;
 
@@ -71,22 +101,38 @@ int main(int argc, const char *argv[]) {
     }
 
     fprintf(stderr, "Total words: %ji\n", wordCount);
+    result.wordsCount = wordCount;
+    result.data = text;
+    result.words = words;
 
-    for (int cnt = 0; cnt < totalRequests; cnt++) {
+    return result;
+}
+
+void writeTestsToFile(const char *fileName, text_t text, int tests, float foundPercent) {
+    FILE *output = fopen(fileName, "w");
+    if (!output) {
+        fprintf(stderr, "Failed to open %s for writing\n", fileName);
+        return;
+    }
+
+    setvbuf(output, NULL, _IOFBF, BUFFERING_SIZE);
+
+    for (int cnt = 0; cnt < tests; cnt++) {
         float normalRand = (float) rand() / RAND_MAX;
         bool wordFromText = normalRand <= foundPercent;
 
         if (wordFromText) {
-            int idx = rand() % wordCount;
-            fprintf(output, "%s\n", words[idx]);
+            int idx = rand() % text.wordsCount;
+            fputs(text.words[idx], output);
         } else {
             char word[MAX_WORD_LEN+1];
-            generateRandomWord(word, 3, 14);
-            fprintf(output, "%s\n", word);
+            generateRandomWord(word, MIN_RNDWORD_LEN, MAX_RNDWORD_LEN);
+            fputs(word, output);
         }
+
+        fputc('\n', output);
     }
 
     fclose(output);
-
-    return 0;
+    return;
 }
